1f21eb3fb49fd3b57046eaaa8b1198d05e19cd70
EtienneK2004 <etienne.kita77@gmail.com>
2025-09-09 12:16:18 +0200

    Question 1-2-3

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -1,7 +1,43 @@
 package com.github.forax.framework.mapper;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public final class JSONWriter {
+  private static class PropertyClassValue extends ClassValue<List<Property>> {
+
+    @Override
+    protected List<Property> computeValue(Class<?> type) {
+      return Arrays.stream(Utils.beanInfo(type).getPropertyDescriptors())
+          .filter(propertyDescriptor -> !"class".equals(propertyDescriptor.getName()))
+          .map(propertyDescriptor ->
+              new Property('"' + propertyDescriptor.getName() + "\": ", propertyDescriptor.getReadMethod()))
+          .toList();
+    }
+  }
+
+  private static final ClassValue<List<Property>> CLASS_VALUE = new PropertyClassValue();
+
+  private record Property(String key, Method method) {}
+
+
   public String toJSON(Object o) {
-    throw new UnsupportedOperationException("TODO");
+    return switch(o) {
+      case null -> "null";
+      case Boolean bool -> "" + bool;
+      case Integer integer -> "" + integer;
+      case Double doub -> "" + doub;
+      case String str -> '"' + str + '"';
+      default -> beanToJSON(o);
+    };
+  }
+
+  private String beanToJSON(Object o) {
+    var properties = CLASS_VALUE.get(o.getClass());
+    return properties.stream()
+        .map(property-> property.key() + toJSON(Utils.invokeMethod(o, property.method())))
+        .collect(Collectors.joining(", ", "{", "}"));
   }
 }
